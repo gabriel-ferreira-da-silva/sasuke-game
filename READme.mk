c9864810b42f51144bfa18dc455edd343e0c52fc
gabriel-ferreira-da-silva <gfs4@cin.ufpe.br>
2021-12-30 14:31:21 -0300

    readme adicionado

--- READme.mk
+++ READme.mk
@@ -11,8 +11,8 @@ poderes: [1,2,3] , [4,5,6]
 
 ## poder 1
 
-![poder 1](.images/s1.png)
+![poder 1](images/s1.png)
 
 ## poder 2
 
-![poder 2](/home/gabriel/Desktop/PROGRAMACAO/PROJETO_NARUTO/s2.png)
+![poder 2](images/s2.png)
