c3edd999c3b144ce9739243819cc2db234f730f8
gabriel-ferreira-da-silva <gfs4@cin.ufpe.br>
2021-12-30 14:36:26 -0300

    readme adicionado

--- READme.mk
+++ READme.mk
@@ -2,11 +2,11 @@
 
 um pequeno minigame de sprites
 com controle de movimentação,
-poderes e combate
+poderes e combate<br>
 
-controle: A,W,S,D
-soco: k
-poderes: [1,2,3] , [4,5,6]
+controle: A,W,S,D<br>
+soco: k<br>
+poderes: [1,2,3] , [4,5,6]<br>
 
 
 ## poder 1
