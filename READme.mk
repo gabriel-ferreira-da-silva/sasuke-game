4e4971ab355fff217e0cbe2e6863efc7c78ea3ae
gabriel-ferreira-da-silva <gfs4@cin.ufpe.br>
2021-12-30 14:29:51 -0300

    readme adicionado

--- READme.mk
+++ READme.mk
@@ -2,16 +2,16 @@
 
 um pequeno minigame de sprites
 com controle de movimentação,
-poderes e combate.
+poderes e combate
 
-controle: A,W,S,D.
-soco: k.
-poderes: [1,2,3] , [4,5,6].
+controle: A,W,S,D
+soco: k
+poderes: [1,2,3] , [4,5,6]
 
 
 ## poder 1
 
-![poder 1](/home/gabriel/Desktop/PROGRAMACAO/PROJETO_NARUTO/s1.png)
+![poder 1](.images/s1.png)
 
 ## poder 2
 
